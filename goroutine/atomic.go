009bfd2d9791b749a77655c7db80004a96246eb4
lujet <462546997@qq.com>
2021-12-09 17:00:06 +0800

    单例模式, sync.Once

--- goroutine/atomic.go
+++ goroutine/atomic.go
@@ -1,33 +1,28 @@
 package main
 
 /*
- * 原子操作是并发编程中"最小的且不可并行化"的操作, 通常如果多个并发体对同一个
- * 共享资源进行的操作是原子操作, 那么同一时刻最多只能有一个并发体对该资源进行
- * 操作; 从线程角度看, 在当前线程修改共享资源期间, 其他线程是不能访问该资源的,
- * 原子操作对多线程并发编程模型来说, 不会发生有别于单线程的意外情况, 共享资源
- * 的完整性可以得到保证.
+ * 用互斥锁保保护一个数值型的共享资源繁琐且效率低下; 标准库的 sync/atomic 包
+ * 对原子操作提供了丰富的支持, 重新实现 ./mutex.go
  *
- * 一般, 原子操作都是通过"互斥"访问来保证的, 通常是由特殊的 CPU 指令提供保护,
- * 如果仅仅想模拟粗粒度的原子操作, 可以借助于 sync.Mutex 实现:
  */
 
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
-var total struct {
-	sync.Mutex
-	value int
-}
+var total uint64
 
 func worker(wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for i := 0; i <= 100; i++ {
-		total.Lock()
-		total.value += i
-		total.Unlock()
+	var i uint64
+	for i = 0; i <= 1000; i++ {
+		// for 循环中使用外部变量, 避免短变量声(:=) 捕获 i 的类型(int)
+		atomic.AddUint64(&total, i)
+		// AddUint64 函数保证了 total 的读取, 更新和保存是一个原子操作,
+		// 因此在多线程中访问也是安全的
 	}
 }
 
@@ -38,5 +33,5 @@ func main() {
 	go worker(&wg)
 	wg.Wait()
 
-	fmt.Println(total.value)
+	fmt.Println(total)
 }
---
+++ goroutine/singletion.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+/*
+ * 原子操作配合互斥锁可以实现高效的单件模式, 互斥锁的代价比普通整数的原子读写
+ * 高很多, 在性能敏感的地方可以增加一个数字型的标志位, 通过原子检测标志位状态
+ * 降低互斥锁的使用次数来提高性能.
+ *
+ *
+ *  TODO:
+ * 单件模式(又称单例模式)确保一个类只有一个实例，并提供一个全局访问点;
+ * 一种用于确定整个应用程序中只有一个类实例且这个实例所占资源在整个应用
+ * 程序中是共享时的程序设计方法.
+ * 使用场景:
+ * - 当类只有一个实例, 且客户可以从一个众所周知的访问点访问它时.
+ * - 当这个唯一实例应该是通过子类化可扩展的, 并且客户应该无需更改代码就能
+ * 使用一个扩展的实例时.
+ */
+
+type singleton struct{}
+
+// var (
+// 	instance    *singleton
+// 	initialized uint32
+// 	mu          sync.Mutex
+// )
+
+// func Instance() *singleton {
+// 	if atomic.LoadUint32(&initialized) == 1 {
+// 		return instance
+// 	}
+
+//  // 对读 instance 操作加锁
+// 	mu.Lock()
+// 	defer mu.Unlock()
+
+// 	if instance == nil {
+// 		defer atomic.StoreUint32(&initialized, 1)
+// 		instance = &singleton{}
+// 	}
+// 	return instance
+// }
+
+// 将通用的代码提取出来就成了标准库中 sync.Once 的实现:
+type Once struct {
+	m    sync.Mutex
+	done uint32
+}
+
+func (o *Once) Do(f func()) {
+	if atomic.LoadUint32(&o.done) == 1 {
+		return
+	}
+
+	// 对读 o.done 加锁
+	o.m.Lock()
+	defer o.m.Unlock()
+
+	if o.done == 0 {
+		defer atomic.StoreUint32(&o.done, 1)
+		f()
+	}
+}
+
+// 基于 sync.Once 重新实现单件(singleton) 模式:
+var (
+	instance *singleton
+	once     sync.Once
+)
+
+func Instance() *singleton {
+	once.Do(func() {
+		instance = &singleton{}
+	})
+
+	return instance
+}
+
+/*
+ * sync/atomic 包对基本数值类型以及复杂对象的读写都提供了原子操作的支持,
+ * atomic.Value 原子对象提供了 Load() 和 Store() 两个原子方法, 分别用于
+ * 加载和保存数据, 返回值和参数都是 interface{} 类型, 因此可以用于任意的
+ * 自定义复杂类型.
+ */
+
+var config atomic.Value
+
+// 这是一个简化的生产者消费者模型, 后台 goroutine 生成最新的配置信息, 前台多个
+// goroutine 获取最新的配置信息, 所有 goroutine 共享配置信息资源.
+func main() {
+	// 初始化配置信息
+	config.Store(loadConfig())
+
+	// 启动一个后台 goroutine, 加载更新后的配置信息
+	go func() {
+		for {
+			time.Sleep(time.Second) // 一秒更新以此配置
+			config.Store(loadConfig())
+		}
+	}()
+	for i := 0; i < 10; i++ {
+		go func() {
+			// for r := range requests() {
+			// 	c := config.Load()
+			// ...
+			// }
+		}()
+	}
+
+}
+
+func loadConfig() struct{} {
+	// ...
+	return struct{}{}
+}
+
+func requests() chan<- struct{} {
+	// ...
+	return nil
+}
