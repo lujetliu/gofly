e1f5dae5c0780e257c5aaa448753266e75d11c5e
lujet <462546997@qq.com>
2022-01-07 22:20:03 +0800

    httprouter 压缩检索树, 结点

---
+++ web/router/go.mod
@@ -0,0 +1,5 @@
+module router
+
+go 1.15
+
+require github.com/julienschmidt/httprouter v1.3.0
--- web/router/httprouter.go
+++ web/router/httprouter.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
 /*
  * go 的 net/http 包提供了基础的路由函数组合与丰富的功能函数, 如果项目的路由
  * 在个位数, URI 固定且不通过 URI 来传递参数的情况下使用 http 提供的默认路由
@@ -125,4 +131,244 @@ const (
   现在每个结点上可以存储字符串了, 这也是"压缩"的含义, 使用压缩检索树可以减少
   树的层数, 同时因为每个结点上的数据存储也比通常的检索树更多, 所以程序的局部
   性更好(一个结点的路径加载到缓存即可进行多个字符的对比), 从而对 cpu 友好.
+*/
+
+// 压缩动态检索树创建过程
+// 路由设定如下:
+// - PUT "/user/installations/:installation_id/repositories/:reposit"
+//
+// - GET /marketplace_listing/plans
+// - GET /marketplace_listing/plans/:id/accounts
+// - GET /search
+// - GET /status
+// - GET /support
+//
+
+/* 根结点创建 ---------------------------------------------------------------*/
+/*
+	httprouter 的 Router 结构体中存储压缩检索树使用数据结构:
+	type Router struct {
+		trees map[string]*node
+		// ...
+	}
+
+	trees 字段的键为 HTTP1.1 的 RFC 中定义的各种方法:
+	GET
+	HEAD
+	OPTIONS
+	POST
+	PUT
+	PATCH
+	DELETE
+
+	每一种方法对应的都是一棵独立的压缩检索树, 这些树彼此之间不共享数据
+
+*/
+
+func main() {
+	r := httprouter.New()
+	r.PUT("/user/installations/:installation_id/repositories/:reposit",
+		func(http.ResponseWriter, *http.Request, httprouter.Params) {})
+	// ...
+	// r.tress 字段未导出, 如何获取压缩检测树的结点? TODO
+}
+
+// PUT 对应的根结点会被创建出来, 其中:
+// radix(计数根) 的结点类型为 *httprouter.node
+// path: 当前结点对应的路径中的字符串
+// wildChild: 子结点是否为参数结点, 即 :id 这种类型的结点
+// nType: 当前结点的类型, 4个枚举值
+// - static: 非根结点的普通字符串结点
+// - root: 根结点
+// - param: 参数结点, 如 :id
+// - catchAll: 通配符结点, 如 *anyway
+// indices: 子结点索引, 当子结点为非参数类型, 即本结点的 wildChild 为 false
+// 时, 会将每个子结点的首字母放入该索引数组(字符串)
+/*
+    PUT 树
+
+	path: "/user/installations/"
+	wildChild: true(子结点为参数结点)
+	nType: root
+	indices: ""
+
+
+
+	path: ":installation_id"
+	wildChild: false
+	nType: param
+	indices: ""
+
+
+
+	path: "/repositories/"
+	wildChild: true
+	nType: default (TODO:?)
+	indices: ""
+
+
+
+	path: ":reposit"
+	wildChild: false
+	nType: param
+	indices: ""
+
+*/
+
+/* 子结点插入 ---------------------------------------------------------------*/
+// - GET /marketplace_listing/plans, 此路由没有参数, 所以 path 都被存储到根结点
+/*
+
+    GET 树
+
+	path: "/marketplace_listing/plans/" (TODO:不需要最后的 / ?)
+	wildChild: false
+	nType: root
+	indices: ""
+
+*/
+
+// - GET /marketplace_listing/plans/:id/accounts, 在 GET 树中和上条路径有相同
+// 的前缀, 所以作为上条路径的子节点进行插入
+/*
+
+    GET 树
+
+	path: "/marketplace_listing/plans/"
+	wildChild: true(注意子结点插入前后的变化)
+	nType: root
+	indices: ""
+
+	path: ":id"
+	wildChild: false
+	nType: param
+	indices: ""
+
+	path: "/accounts"
+	wildChild: false
+	nType: default
+	indices: ""
+
+	// 由于 :id 这个结点是只有一个字符串的普通子结点, 因此 indices 不需要处理
+
+*/
+
+/* 边分裂 ------------------------------------------------------------------*/
+// - GET /search, 会导致树的边分裂
+
+/*
+    GET 树
+
+	path: "/"
+	wildChild: false
+	nType: root
+	indices: "ms"
+
+		|             \
+		|              \
+		|               \
+		|                \
+		|                 \|
+		|
+		|                path: "search"
+		|                wildChild: false
+		|                nType: default
+		|                indices: ""
+		|
+	   \|/
+
+	path: "marketplace_listing/plans/"
+	wildChild: true
+	nType: root
+	indices: ""
+		|
+		|
+	   \|/
+	path: ":id"
+	wildChild: false
+	nType: param
+	indices: ""
+		|
+		|
+	   \|/
+	path: "/accounts"
+	wildChild: false
+	nType: default
+	indices: ""
+
+	原有路径和新的路径在初始的 / 位置发生分裂, 这样就需要把原有的根结点内容下
+	移, 再将新路由 search 同样作为子结点挂在根结点之后; 此时因为出现多个子结点,
+	根结点的 indices 提供子结点索引, 其值 "ms" 代表子结点的首字母, 分别为
+	m(marketplace_listing), s(search)
+
+*/
+
+// 继续插入以下子结点
+// - GET /status
+// - GET /support
+/*
+    GET 树
+
+	path: "/"
+	wildChild: false
+	nType: root
+	indices: "ms"
+
+		|             \
+		|              \                            ------->  path: "earch"
+		|               \                          |          wildChild: false
+		|                \                         |          nType: default
+		|                 \|                       |          indices: ""
+		|                                          |
+		|                path: "s"                 |
+	 	|                wildChild: false          |         path: "tatus"
+		|                nType: default   ---------|----->   wildChild: false
+		|                indices: "etu"            |         nType: default
+		|                                          |         indices: ""
+	   \|/                                          \
+										             \
+	path: "marketplace_listing/plans/"                \
+	wildChild: true                                   |
+	nType: root                                      \|/
+	indices: ""                                 path: "upport"
+	    |                                       wildChild: false
+		|                                       nType: default
+	   \|/                                      indices:""
+	path: ":id"
+	wildChild: false
+	nType: param
+	indices: ""
+		|
+		|
+	   \|/
+	path: "/accounts"
+	wildChild: false
+	nType: default
+	indices: ""
+
+	原有路径和新的路径在初始的 / 位置发生分裂, 这样就需要把原有的根结点内容下
+	移, 再将新路由 search 同样作为子结点挂在根结点之后; 此时因为出现多个子结点,
+	根结点的 indices 提供子结点索引, 其值 "ms" 代表子结点的首字母, 分别为
+	m(marketplace_listing), s(search)
+
+*/
+
+/* 子结点冲突处理 -----------------------------------------------------------*/
+// 只要发生冲突, 在初始化阶段就会 panic
+/*
+   在路由本身只有字符串的情况下, 不会发生任何冲突; 只有当路由中含有通配符(如:id)
+   或 catchAll 的情况下才可能发生冲突, 冲突处理分几种情况:
+   - 在插入 wildChild 时父结点的 children 数组非空且 wildChild 被设置为 false.
+	   如: GET /user/getAll 和 GET /user/:id/getAddr
+
+   - 在插入 wildChild 结点时, 父结点的 children 数组非空且 wildChild 被设置为
+	 true, 但该父结点的已存在的 wildChild 子结点与即将插入的 wildChild 名字不
+	 一样. 如: GET /user/:id/info 和 GET /user/:name/info
+
+  - 在插入 catchAll 结点时, 父结点的 children 非空.
+		如: GET /src/abc 和 GET /src/*filename
+
+  - 在插入 static 结点时, 父结点的 wildChild 字段被设置为 true.
+
+  - 在插入 static 结点时, 父结点的 children 非空, 同时子结点 nType 为 catchAll
 */
